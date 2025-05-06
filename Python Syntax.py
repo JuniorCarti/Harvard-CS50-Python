52a99f1aaa58977c033fbc8136628952376bc791
JuniorCarti <ridgejunior204@gmail.com>
2025-05-06 15:52:21 +0300

    Variables

--- Python Syntax.py
+++ Python Syntax.py
@@ -20,6 +20,11 @@ if 5 > 2:
 # Variables can be assigned values using the assignment operator (=).
 # The variable name can be any combination of letters, numbers, and underscores, but it cannot start with a number.
 # Variable names are case-sensitive, meaning that "myVar" and "myvar" are considered different variables.
+#Python Variables
+x = 5
+y = "Hello World" 
+print(x)
+print(y)  # integer
 # Variable names should be descriptive and meaningful to make the code more readable.
 # It is a good practice to use lowercase letters for variable names and separate words with underscores (e.g., my_variable).
 # However, it is also common to use camelCase (e.g., myVariable) in some coding styles.
