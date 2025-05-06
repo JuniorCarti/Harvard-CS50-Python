c737e220152e9c3611c707f26d5a3707c78a0174
JuniorCarti <ridgejunior204@gmail.com>
2025-05-06 15:56:59 +0300

    Updated variables

--- variables.py
+++ variables.py
@@ -6,3 +6,9 @@
 # # The variable name can be any combination of letters, numbers, and underscores, but it cannot start with a number.
 # # Variable names are case-sensitive, meaning that "myVar" and "myvar" are considered different variables.
 # # Python Variables
+x = 5
+y = "Hello World"
+print(x)
+print(y)  # integer
+# Variable names should be descriptive and meaningful to make the code more readable.
+# It is a good practice to use lowercase letters for variable names and separate words with underscores (e.g., my_variable).
