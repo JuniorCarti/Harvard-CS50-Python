2b430fd67e367c11a3a744e0959ab4ad43165333
JuniorCarti <ridgejunior204@gmail.com>
2025-05-06 14:23:54 +0300

    Added a first python program calculator.py

---
+++ calculator.py
@@ -0,0 +1,15 @@
+#integer data types
+#x = int(input("What's x? "))  # nested functions
+#y = int(input("What's y? "))
+
+#z = int(x) + int(y)
+
+#print(x + y)
+
+#floating data types
+x = float(input("What's x? "))
+y = float(input("What's y? "))
+
+z = int(x) + int(y)
+
+print(x + y)
