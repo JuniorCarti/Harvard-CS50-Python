ebc3948b98c4113aa765eff78a1800cb714a6be5
JuniorCarti <ridgejunior204@gmail.com>
2025-05-06 15:56:02 +0300

    Python variables

---
+++ variables.py
@@ -0,0 +1,8 @@
+#variables  
+# # Variables are containers for storing data values.
+# # In Python, variables do not need to be declared with any particular type and can even change type after they have been set.
+# # Variables do not need to be declared with any particular type and can even change type after they have been set.
+# # Variables can be assigned values using the assignment operator (=).
+# # The variable name can be any combination of letters, numbers, and underscores, but it cannot start with a number.
+# # Variable names are case-sensitive, meaning that "myVar" and "myvar" are considered different variables.
+# # Python Variables
