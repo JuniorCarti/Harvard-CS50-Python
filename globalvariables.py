531081bccc07362ec911b1fac186478e5e618ed0
JuniorCarti <ridgejunior204@gmail.com>
2025-05-07 12:26:57 +0300

    Updated global variables

---
+++ globalvariables.py
@@ -0,0 +1,7 @@
+#Global Variables 
+#This file contains all the global variables used in the project
+#It is used to store the global variables that are used in the project
+#Create a variable outside of a function, and use it inside the function
+#Global variables are variables that are declared outside of a function and can be accessed inside the function
+#Global variables are declared using the global keyword
+#Global variables are used to store data that can be accessed by all functions in the project
