8ff43075aba711b5b12565553ffb0bef2082588e
JuniorCarti <ridgejunior204@gmail.com>
2025-05-07 12:40:39 +0300

    Data types

---
+++ datatypes.py
@@ -0,0 +1,15 @@
+#Built-in Data Types
+# Python has the following built-in data types:
+#
+# 1. Text Type:	str
+# 2. Numeric Types:	int, float
+# 3. Sequence Types:	list, tuple, range  
+#4. Mapping Type:	dict
+# 5. Set Types:	set, frozenset
+# 6. Boolean Type:	bool
+# 7. Binary Types:	bytes, bytearray, memoryview
+# 8. None Type:	NoneType
+#
+# 9. User-defined Types:	Custom classes and objects
+# 10. Enumerations:	Enum classes
+# 11. Data Classes:	Dataclasses for creating classes with built-in methods
