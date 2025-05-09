a714b756a53156adeed67076709c88ee10eba136
JuniorCarti <ridgejunior204@gmail.com>
2025-05-09 15:03:52 +0300

    Add string examples using single and double quotes in Python

---
+++ python strings.py
@@ -0,0 +1,9 @@
+#Strings in python are surrounded by either single quotation marks, or double quotation marks.
+# You can display a string literal with the print() function:
+# x = "Hello World"
+# y = 'Hello World'
+# print(x)
+
+x= "Hello World"
+y= 'Hello World'
+print(x)
