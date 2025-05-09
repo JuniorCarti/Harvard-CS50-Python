9464d1e33f914879e6a48a24c665cb7dd17eafd4
JuniorCarti <ridgejunior204@gmail.com>
2025-05-09 15:52:08 +0300

    Python Modifying Strings UpperCase

---
+++ pythonmodifystrings.py
@@ -0,0 +1,9 @@
+#Python has a set of built-in methods that you can use on strings.
+#Upper Case
+#The upper() method returns the string in upper case
+#x = "Hello World"
+#y = "Hello World"
+x = "Hello World"
+y = "Hello World"
+print(x.upper())
+print(y.upper())
