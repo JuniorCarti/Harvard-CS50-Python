69b66096d452e1f6ed084aaa1a4ad10aa3d56459
JuniorCarti <ridgejunior204@gmail.com>
2025-05-12 16:21:21 +0300

    Python operators

--- pythonoperators.py
+++ pythonoperators.py
@@ -1,3 +1,12 @@
 #Python Operators
 #Python Operators are special symbols that perform operations on variables and values.
 #Python Operators are used to perform operations on variables and values.
+#In the example below, we use the + operator to add together two values:
+#x = 5
+#y = 10
+#z = x + y
+# print(z)
+x = 5
+y = 10
+z = x + y
+print(z)
