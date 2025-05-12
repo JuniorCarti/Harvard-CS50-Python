0884fb4990fbabe5daa88768eeab7a477a8a72ee
JuniorCarti <ridgejunior204@gmail.com>
2025-05-12 16:23:21 +0300

    Python operators

--- pythonoperators.py
+++ pythonoperators.py
@@ -10,3 +10,25 @@ x = 5
 y = 10
 z = x + y
 print(z)
+
+#Python Arithmetic Operators
+#Python Arithmetic Operators are used to perform arithmetic operations on variables and values.
+#subtraction
+#addition
+#multiplication
+#division
+#modulus
+
+#exponentiation
+#floor division
+#Python Arithmetic Operators are used to perform arithmetic operations on variables and values.
+#In the example below, we use the + operator to add together two values:
+x = 5
+y = 10
+z = x + y
+print(z)
+#In the example below, we use the - operator to subtract two values:
+x = 5
+y = 10
+z = x - y
+print(z)
