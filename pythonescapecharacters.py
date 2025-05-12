690b4998732c0bec0770545305eeb1f83810bfeb
JuniorCarti <ridgejunior204@gmail.com>
2025-05-12 14:53:50 +0300

    Python Escape Strings

--- pythonescapecharacters.py
+++ pythonescapecharacters.py
@@ -4,3 +4,21 @@
 #Escape characters are often used to represent special characters such as newlines, tabs, and quotes.
 #Escape characters are also used to represent characters that have a special meaning in Python strings, such as backslashes and quotes.
 #Escape characters are often used in string formatting to create dynamic strings.
+#You will get an error if you use double quotes inside a string that is surrounded by double quotes:
+
+txt = "We are the so-called "Vikings" from the north."
+#You will get an error if you use single quotes inside a string that is surrounded by single quotes:
+txt = 'We are the so-called 'Vikings' from the north.'
+#You will get an error if you use backslashes inside a string that is surrounded by backslashes:
+txt = "We are the so-called \Vikings\ from the north."
+#You will get an error if you use newlines inside a string that is surrounded by newlines:
+txt = "We are the so-called
+Vikings
+from the north."
+#You will get an error if you use tabs inside a string that is surrounded by tabs:
+txt = "We are the so-called
+    Vikings
+    from the north."
+#You will get an error if you use quotes inside a string that is surrounded by quotes:
+txt = "We are the so-called "Vikings" from the north."
+#You will get an error if you use quotes inside a string that is surrounded by quotes:
