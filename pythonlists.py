5406c55712a14c97b773985f412ba129b7da916b
JuniorCarti <ridgejunior204@gmail.com>
2025-05-12 16:43:47 +0300

    Python lists

--- pythonlists.py
+++ pythonlists.py
@@ -2,3 +2,7 @@
 # #python lists are used to store multiple values in a single variable.
 mylist = ["apple", "banana", "cherry"]
 print(mylist)
+
+#Lists are created using square brackets:
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
