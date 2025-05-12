87bc2a7d9a5d8f2dd77f915dfc6b242c465b23c9
JuniorCarti <ridgejunior204@gmail.com>
2025-05-12 16:44:40 +0300

    Python lists

--- pythonlists.py
+++ pythonlists.py
@@ -6,3 +6,8 @@ print(mylist)
 #Lists are created using square brackets:
 thislist = ["apple", "banana", "cherry"]
 print(thislist)
+
+#Lists are ordered, changeable, and allow duplicate values.
+#Lists are ordered, meaning that the items have a defined order, and that order will not change.
+#When you create a list, you will always get the same order.
+#If you add new items to a list, the new items will be placed at the end of the list.
