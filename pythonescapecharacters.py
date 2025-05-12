8d13d881999f8ba27f59330a7c8ca665d305ef1d
JuniorCarti <ridgejunior204@gmail.com>
2025-05-12 14:55:11 +0300

    Python Escape Strings

--- pythonescapecharacters.py
+++ pythonescapecharacters.py
@@ -6,19 +6,23 @@
 #Escape characters are often used in string formatting to create dynamic strings.
 #You will get an error if you use double quotes inside a string that is surrounded by double quotes:
 
-txt = "We are the so-called "Vikings" from the north."
+#txt = "We are the so-called "Vikings" from the north."
 #You will get an error if you use single quotes inside a string that is surrounded by single quotes:
-txt = 'We are the so-called 'Vikings' from the north.'
+#txt = 'We are the so-called 'Vikings' from the north.'
 #You will get an error if you use backslashes inside a string that is surrounded by backslashes:
-txt = "We are the so-called \Vikings\ from the north."
+#txt = "We are the so-called \Vikings\ from the north."
 #You will get an error if you use newlines inside a string that is surrounded by newlines:
-txt = "We are the so-called
-Vikings
-from the north."
+#txt = "We are the so-called
+#Vikings
+#from the north."
 #You will get an error if you use tabs inside a string that is surrounded by tabs:
-txt = "We are the so-called
-    Vikings
-    from the north."
+##   Vikings
+  #  from the north."
 #You will get an error if you use quotes inside a string that is surrounded by quotes:
-txt = "We are the so-called "Vikings" from the north."
+#txt = "We are the so-called "Vikings" from the north."
 #You will get an error if you use quotes inside a string that is surrounded by quotes:
+#to solve this problem, you can use escape characters.
+#Escape characters are characters that have a special meaning in Python strings.
+#Escape characters are used to represent characters that cannot be typed directly into a string.
+txt = "We are the so-called \"Vikings\" from the north."
+print(txt)
