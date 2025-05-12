580542e69f778f5bbf673e0250c882763957bd4c
JuniorCarti <ridgejunior204@gmail.com>
2025-05-12 14:43:40 +0300

    Python string format placeholders and modifiers

--- pythonformatstrings.py
+++ pythonformatstrings.py
@@ -230,6 +230,17 @@ y = 10
 txt = "The sum of {} and {} is {}".format(x, y, x + y)
 print(txt)
 
+#A placeholder can contain Python code, like math operations:
+x = 5
+y = 10
+txt = "The sum of {0} and {1} is {2}".format(x, y, x + y)
+print(txt)
+
+x = 5
+y = 10
+txt = "The sum of {x} and {y} is {sum}".format(x=x, y=y, sum=x + y)
+print(txt)
+
 
 
 
