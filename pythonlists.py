baa93d43941d347f1778e6f1409106d5bf6a7c4f
JuniorCarti <ridgejunior204@gmail.com>
2025-05-14 16:31:35 +0300

    Python join lists

--- pythonlists.py
+++ pythonlists.py
@@ -411,3 +411,9 @@ list2 = ["orange", "kiwi", "mango"]
 list1.append(list2)
 print(list1)
 
+#extend two lists
+list1 = ["apple", "banana", "cherry"]
+list2 = ["orange", "kiwi", "mango"]
+list1.extend(list2)
+print(list1)
+
