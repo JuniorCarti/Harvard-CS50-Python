1e7502e46b0e702807e43f1971c979a384d16c81
JuniorCarti <ridgejunior204@gmail.com>
2025-05-14 16:40:03 +0300

    Python tuples

---
+++ pythontuples.py
@@ -0,0 +1,5 @@
+#python tuples
+# Tuples are immutable sequences, typically used to store collections of heterogeneous data.
+# Tuples are defined by enclosing the elements in parentheses ().
+# Tuples can contain any type of data, including other tuples.
+# Tuples are often used to return multiple values from a function.
