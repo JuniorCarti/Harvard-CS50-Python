e2d6ef11bc5c889f8c8bda97b31897d559a08d5f
JuniorCarti <ridgejunior204@gmail.com>
2025-05-14 15:52:24 +0300

    Python lists

--- pythonlists.py
+++ pythonlists.py
@@ -161,3 +161,10 @@ print(thislist)
 thislist = ["apple", "banana", "cherry", "banana"]
 thislist.remove("banana")
 print(thislist)
+
+
+#pop() method
+#The pop() method removes the specified index, (or the last item if index is not specified):
+thislist = ["apple", "banana", "cherry"]
+thislist.pop(1)
+print(thislist)
