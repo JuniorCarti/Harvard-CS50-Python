2470a8a7caf0be37d1aef2ed1a10cd03d922710c
JuniorCarti <ridgejunior204@gmail.com>
2025-05-15 15:22:21 +0300

    Python tuples

--- pythontuples.py
+++ pythontuples.py
@@ -94,6 +94,9 @@ print(numbers[1:4])
 print(numbers[4:8])
 print(numbers[:4])
 print(numbers[4:])
+print(numbers[::4]) ## Output: (1, 3, 5, 7, 9) (every fourth item)
+print(numbers[4::])
+print(numbers[::-1])
 
 
 
