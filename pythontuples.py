502c69a3ddfe03f38bb781230b32e54b7309f2dc
JuniorCarti <ridgejunior204@gmail.com>
2025-05-16 13:07:44 +0300

    Python tuple methods

--- pythontuples.py
+++ pythontuples.py
@@ -432,6 +432,14 @@ for grade in grades:
         if student[2] == grade:
             count += 1
     print(f"Number of students with grade {grade}: {count}")
+
+
+#Python - Tuple Methods
+#Python has two built-in methods that you can use on tuples.
+#Method	Description
+#count()	Returns the number of times a specified value occurs in a tuple
+#index()	Searches the tuple for a specified value and returns the position of where it was found
+
     
 
 
