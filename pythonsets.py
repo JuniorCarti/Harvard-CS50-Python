0aa93e81b484a85b44e549385fab2339d57c1803
JuniorCarti <ridgejunior204@gmail.com>
2025-05-19 11:45:56 +0300

    Python sets

--- pythonsets.py
+++ pythonsets.py
@@ -6,3 +6,8 @@ myset = {"Benz", "Rav4", "Volvo", "AudiR3 Sportback"}
 #Sets are written with curly brackets.
 print(myset)
 #Note: Sets are unordered, so you cannot be sure in which order the items will appear.
+#Set Items
+#Set items are unordered, unchangeable, and do not allow duplicate values.
+#Unordered
+#Unordered means that the items in a set do not have a defined order.
+#Set items can appear in a different order every time you use them, and cannot be referred to by index or key.
