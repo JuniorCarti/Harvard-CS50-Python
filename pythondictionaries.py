c45a6bf18b13302b83f68ea9a19ae83122c2c18a
JuniorCarti <ridgejunior204@gmail.com>
2025-05-20 10:40:29 +0300

    Python Dictionaries

--- pythondictionaries.py
+++ pythondictionaries.py
@@ -13,3 +13,13 @@ print(thisdict)
 #Dictionary items are ordered, changeable, and do not allow duplicates.
 #Dictionary items are presented in key:value pairs, and can be referred to by using the key name.
 print(thisdict["brand"])
+
+#Duplicates Not Allowed
+#Dictionaries cannot have two items with the same key:
+#Duplicate values will overwrite existing values:
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964,
+  "year": 2023
+}
