3ea0cdf1fcd11ff2888fadf5fed9174fa198edde
JuniorCarti <ridgejunior204@gmail.com>
2025-05-20 10:36:53 +0300

    Python Dictionaries

--- pythondictionaries.py
+++ pythondictionaries.py
@@ -3,3 +3,9 @@ thisdict = {
   "model": "Mustang",
   "year": 1964
 }
+#dictionary
+#Dictionaries are used to store data values in key:value pairs.
+#A dictionary is a collection which is ordered*, changeable and do not allow duplicates.
+#Dictionaries are written with curly brackets, and have keys and values:
+
+print(thisdict)
