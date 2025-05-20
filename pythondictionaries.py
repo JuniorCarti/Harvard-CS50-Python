3b9aec2c92192285ef17fc78fe831b9090c08bdc
JuniorCarti <ridgejunior204@gmail.com>
2025-05-20 10:38:35 +0300

    Python Dictionaries

--- pythondictionaries.py
+++ pythondictionaries.py
@@ -9,3 +9,7 @@ thisdict = {
 #Dictionaries are written with curly brackets, and have keys and values:
 
 print(thisdict)
+#Dictionary Items
+#Dictionary items are ordered, changeable, and do not allow duplicates.
+#Dictionary items are presented in key:value pairs, and can be referred to by using the key name.
+print(thisdict["brand"])
