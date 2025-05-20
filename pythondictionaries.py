b907fff6e4a387dbc55ae1f28553836336a4ad8b
JuniorCarti <ridgejunior204@gmail.com>
2025-05-20 10:45:23 +0300

    Python Dictionaries

--- pythondictionaries.py
+++ pythondictionaries.py
@@ -38,3 +38,9 @@ thisdict = {
 }
 print(thisdict)
 print(type(thisdict))
+
+#The dict() Constructor
+#It is also possible to use the dict() constructor to make a dictionary.
+#Using the dict() method to make a dictionary:
+thisdict = dict(name = "John", age = 36, country = "Norway")
+print(thisdict)
