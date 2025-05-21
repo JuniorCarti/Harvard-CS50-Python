f105318606ad4af7a22ca0098e36d595983f9b0f
JuniorCarti <ridgejunior204@gmail.com>
2025-05-21 10:52:32 +0300

    Python if else statements

--- pythonif-else.py
+++ pythonif-else.py
@@ -22,3 +22,14 @@ if a < b:
     print("a is less than b")
 elif a >= b:
     print("b is greater than a")
+
+#else keyword
+#The else keyword catches anything which isn't caught by the preceding conditions.
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("a is greater than b")
