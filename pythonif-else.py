bdde8d27452db7e4abe54cf1ee533dc66994c4b5
JuniorCarti <ridgejunior204@gmail.com>
2025-05-21 10:55:45 +0300

    Python if else statements

--- pythonif-else.py
+++ pythonif-else.py
@@ -47,3 +47,9 @@ else:
 #Short Hand If
 #If you have only one statement to execute, you can put it on the same line as the if statement.
 if a > b: print("a is greater than b")
+
+#Short Hand If ... Else
+#If you have only one statement to execute, one for if, and one for else, you can put it all on the same line:
+a = 2
+b = 330
+print("A") if a > b else print("B")
