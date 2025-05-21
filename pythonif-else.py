04c357c1ee598710bb31dcb90a1e2b4f3a60d1f3
JuniorCarti <ridgejunior204@gmail.com>
2025-05-21 10:56:57 +0300

    Python if else statements

--- pythonif-else.py
+++ pythonif-else.py
@@ -53,3 +53,8 @@ if a > b: print("a is greater than b")
 a = 2
 b = 330
 print("A") if a > b else print("B")
+#This technique is known as Ternary Operators, or Conditional Expressions.
+#You can also have multiple else statements on the same line:
+a = 330
+b = 330
+print("A") if a > b else print("=") if a == b else print("B")
