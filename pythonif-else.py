c176d38c6b19812bb325926df17564caa6e73a4c
JuniorCarti <ridgejunior204@gmail.com>
2025-05-21 11:00:02 +0300

    Python if else statements

--- pythonif-else.py
+++ pythonif-else.py
@@ -74,3 +74,10 @@ b = 33
 c = 500
 if a > b or a > c:
   print("At least one of the conditions is True")
+
+#Not keyword
+#The not keyword is a logical operator, and is used to reverse the result of the conditional statement:
+a = 33
+b = 200
+if not a > b:
+  print("a is NOT greater than b")
