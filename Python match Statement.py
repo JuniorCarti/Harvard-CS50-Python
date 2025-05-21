c7e4b091c5b7d5503fdf9c4b53293749daea8d36
JuniorCarti <ridgejunior204@gmail.com>
2025-05-21 14:02:49 +0300

    Python match statement

---
+++ Python match Statement.py
@@ -0,0 +1,9 @@
+#Python match Statement
+#Basic sysntax
+"""match subject:
+    case pattern1:
+        # handle pattern1
+    case pattern2:
+        # handle pattern2
+    case _:
+        # default case (like else)"""
