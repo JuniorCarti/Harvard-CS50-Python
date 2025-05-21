46ceda4a674ece611173fa047a30baae349f088a
JuniorCarti <ridgejunior204@gmail.com>
2025-05-21 10:58:59 +0300

    Python if else statements

--- pythonif-else.py
+++ pythonif-else.py
@@ -66,3 +66,11 @@ b = 33
 c = 500
 if a > b and c > a:
   print("Both conditions are True")
+
+#Or keyword
+#The or keyword is a logical operator, and is used to combine conditional statements:
+a = 200
+b = 33
+c = 500
+if a > b or a > c:
+  print("At least one of the conditions is True")
