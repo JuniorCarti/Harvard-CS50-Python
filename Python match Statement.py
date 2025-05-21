99d8ec93211d26f9705d27319ee43c2c4686902d
JuniorCarti <ridgejunior204@gmail.com>
2025-05-21 14:04:51 +0300

    Python match statement

--- Python match Statement.py
+++ Python match Statement.py
@@ -7,3 +7,19 @@
         # handle pattern2
     case _:
         # default case (like else)"""
+#Simple Value Matching
+#Works like a switch-case statement in other languages:
+def http_status(status):
+    match status:
+        case 200:
+            return "OK"
+        case 404:
+            return "Not Found"
+        case 500:
+            return "Server Error"
+        case _:
+            return "Unknown status"
+
+print(http_status(200))  # "OK"
+print(http_status(404))  # "Not Found"
+print(http_status(999))  # "Unknown status"
