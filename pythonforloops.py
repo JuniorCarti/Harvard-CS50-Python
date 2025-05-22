ffaad35c817635c7b1e274b431e57e18f8bbcd55
JuniorCarti <ridgejunior204@gmail.com>
2025-05-22 11:35:52 +0300

    Python for loops

--- pythonforloops.py
+++ pythonforloops.py
@@ -65,7 +65,7 @@ squares = [x**2 for x in range(10)]
 
 #1. Data Processing Pipeline (ETL)
 ## Extract raw data from multiple sources
-raw_data_sources = [
+"""raw_data_sources = [
     'sales_2025.csv',
     'inventory.db',
     'customer_api.json'
@@ -191,11 +191,14 @@ fruits = ["apple", "banana", "cherry"]
 for x in fruits:
   if x == "banana":
     continue
-  print(x)
+  print(x)"""
 
 """The range() Function
 To loop through a set of code a specified number of times, we can use the range() function,
 The range() function returns a sequence of numbers, 
 starting from 0 by default, and increments by 1 (by default), and ends at a specified number."""
-for x in range(6):
+
+
+#Using the start parameter:
+for x in range(2, 6):
   print(x)
