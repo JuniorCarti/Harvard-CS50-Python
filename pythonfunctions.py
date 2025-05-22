cdc1fa73bd747bee51d18e6e44f6d301ebd6844a
JuniorCarti <ridgejunior204@gmail.com>
2025-05-22 12:04:11 +0300

    Python functions

--- pythonfunctions.py
+++ pythonfunctions.py
@@ -11,3 +11,14 @@ def greet(name):
     print(f"Hello, {name}!")
 greet("Alice")
         
+#2. Function Parameters
+#Functions can take different types of parameters:
+#a. Positional Arguments (Required)
+#Must be passed in the correct order.
+def add(a, b):
+    return a + b
+print(add(5, 3))
+
+def multiply(c, d):
+    return c * d
+print(multiply(5, 10))
