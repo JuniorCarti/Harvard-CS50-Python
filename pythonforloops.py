56c32858fab9440b2422415819852c68c92b70a7
JuniorCarti <ridgejunior204@gmail.com>
2025-05-22 10:38:18 +0300

    Python for loops

--- pythonforloops.py
+++ pythonforloops.py
@@ -1,2 +1,7 @@
 #Python For Loops
 #A for loop is used for iterating over a sequence (that is either a list, a tuple, a dictionary, a set, or a string).
+#For loops in Python are control flow statements that allow you to repeatedly execute 
+#a block of code for each item in a sequence (like lists, tuples, strings, etc.). 
+# They follow this basic syntax:
+#for item in iterable:
+    # code to execute
