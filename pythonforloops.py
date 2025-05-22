8c26e35c60bd07fa462b995143bd574150bb4629
JuniorCarti <ridgejunior204@gmail.com>
2025-05-22 11:24:57 +0300

    Python for loops

--- pythonforloops.py
+++ pythonforloops.py
@@ -170,3 +170,11 @@ for x in fruits:
 #Looping Through a String
 for x in "banana":
   print(x)
+
+#The break Statement
+#With the break statement we can stop the loop before it has looped through all the items:
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+  if x == "banana":
+    break
