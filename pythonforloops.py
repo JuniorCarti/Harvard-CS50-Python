becc36b1fdd6d2c93dfd9d0dc8411cdfbad86441
JuniorCarti <ridgejunior204@gmail.com>
2025-05-22 11:39:25 +0300

    Python for loops

--- pythonforloops.py
+++ pythonforloops.py
@@ -216,3 +216,11 @@ for x in range(6):
   print(x)
 else:
   print("Finally finished!")
+
+# The else block will NOT be executed if the loop is stopped by a break statement.
+#Break the loop when x is 3, and see what happens with the else block:
+for x in range(6):
+  if x == 3: break
+  print(x)
+else:
+  print("Finally finished!")
