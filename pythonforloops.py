aab514fd20b693e0f5529dd1c1320a23ca9d8803
JuniorCarti <ridgejunior204@gmail.com>
2025-05-22 11:32:59 +0300

    Python for loops

--- pythonforloops.py
+++ pythonforloops.py
@@ -192,3 +192,10 @@ for x in fruits:
   if x == "banana":
     continue
   print(x)
+
+"""The range() Function
+To loop through a set of code a specified number of times, we can use the range() function,
+The range() function returns a sequence of numbers, 
+starting from 0 by default, and increments by 1 (by default), and ends at a specified number."""
+for x in range(6):
+  print(x)
