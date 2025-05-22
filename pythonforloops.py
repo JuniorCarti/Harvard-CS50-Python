6962678014faf484889c762d0b4ac815f1431681
JuniorCarti <ridgejunior204@gmail.com>
2025-05-22 11:25:46 +0300

    Python for loops

--- pythonforloops.py
+++ pythonforloops.py
@@ -178,3 +178,9 @@ for x in fruits:
   print(x)
   if x == "banana":
     break
+#Exit the loop when x is "banana", but this time the break comes before the print:
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    break
+  print(x)
