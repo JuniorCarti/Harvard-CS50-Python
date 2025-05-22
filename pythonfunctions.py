cae778ec81dec5e6d84d6b9797ff64094646e527
JuniorCarti <ridgejunior204@gmail.com>
2025-05-22 11:57:58 +0300

    Python functions

---
+++ pythonfunctions.py
@@ -0,0 +1,4 @@
+#Functions are reusable blocks of code that perform a specific task. 
+#They help in organizing code, avoiding repetition, and improving readability.
+#1. Function Definition & Syntax
+#A function is defined using the def keyword.
