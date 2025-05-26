c95ae57b16b85632741630bd1459ab972a79ac04
JuniorCarti <ridgejunior204@gmail.com>
2025-05-26 17:49:38 +0300

    Python lambda

--- python_lambda.py
+++ python_lambda.py
@@ -1,2 +1,6 @@
 #A lambda function is a small anonymous function.
 #A lambda function can take any number of arguments, but can only have one expression.
+#Sysntax
+# lambda arguments: expression
+def add(x, y):
+    return x + y
