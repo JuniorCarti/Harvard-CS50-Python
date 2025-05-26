94e85c981eb6ee5070591edd1bbf0a8876260671
JuniorCarti <ridgejunior204@gmail.com>
2025-05-26 17:51:27 +0300

    Python lambda

--- python_lambda.py
+++ python_lambda.py
@@ -13,3 +13,6 @@ print(add_lambda(5, 3))  # Output: 8
 # Lambda functions are often used for short, throwaway functions.
 # They are commonly used in functional programming constructs like map, filter, and reduce.
 # Example of using lambda with map
+numbers = [1, 2, 3, 4, 5]
+squared_numbers = list(map(lambda x: x ** 2, numbers))
+print(squared_numbers)  # Output: [1, 4, 9, 16, 25]
