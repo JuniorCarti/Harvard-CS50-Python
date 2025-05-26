9eeb80a753a1a5042bffa2a7240e1051692ce839
JuniorCarti <ridgejunior204@gmail.com>
2025-05-26 17:51:55 +0300

    Python lambda

--- python_lambda.py
+++ python_lambda.py
@@ -16,3 +16,6 @@ print(add_lambda(5, 3))  # Output: 8
 numbers = [1, 2, 3, 4, 5]
 squared_numbers = list(map(lambda x: x ** 2, numbers))
 print(squared_numbers)  # Output: [1, 4, 9, 16, 25]
+# Example of using lambda with filter
+even_numbers = list(filter(lambda x: x % 2 == 0, numbers))
+print(even_numbers)  # Output: [2, 4]
