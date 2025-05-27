3825060641581d9be1d383c8287162c3eb6293c4
JuniorCarti <ridgejunior204@gmail.com>
2025-05-27 13:12:22 +0300

    python lambda

--- pythonLambda.py
+++ pythonLambda.py
@@ -11,3 +11,7 @@ print(x(5))
 #Multiply argument a with argument b and return the result:
 x = lambda a, b : a * b
 print(x(5, 6))
+
+#Summarize argument a, b, and c and return the result:
+x = lambda a, b, c : a + b + c
+print(x(10, 20, 30))
