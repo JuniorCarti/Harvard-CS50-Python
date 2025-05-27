cd318345b939e20339c3cf8a03c42418192fe0de
JuniorCarti <ridgejunior204@gmail.com>
2025-05-27 14:38:27 +0300

    Python classes and objects

--- PythonClasses_Objects.py
+++ PythonClasses_Objects.py
@@ -9,3 +9,11 @@ class myClass:
 #Create an object named p1, and print the value of x:
 p1 = myClass
 print(p1.x)
+
+#The __init__() Function
+"""The examples above are classes and objects in their simplest form, and are not really useful in real life applications.
+To understand the meaning of classes we have to understand the built-in __init__() function.
+All classes have a function called __init__(), which is always executed when the class is being initiated.
+Use the __init__() function to assign values to object properties, 
+or other operations that are necessary to do when the object is being created:"""
+
