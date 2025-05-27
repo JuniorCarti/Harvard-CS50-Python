4d95fa083390e7abd3bfd6ff7cabe5452b39e0b0
JuniorCarti <ridgejunior204@gmail.com>
2025-05-27 13:11:18 +0300

    python lambda

--- pythonLambda.py
+++ pythonLambda.py
@@ -2,4 +2,12 @@
 #A lambda function can take any number of arguments, but can only have one expression.
 #     return lambda name: f"{greeting}, {name}!"    
 #Basic syntax
-lambda arguments: expression
+#lambda arguments: expression
+#The expression is executed and the result is returned:
+x = lambda a : a + 10
+print(x(5))
+
+#Lambda functions can take any number of arguments:
+#Multiply argument a with argument b and return the result:
+x = lambda a, b : a * b
+print(x(5, 6))
