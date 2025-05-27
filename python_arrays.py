17ff0e146e7b2813fbf0105a63aa1b21eb5b5b1b
JuniorCarti <ridgejunior204@gmail.com>
2025-05-27 13:51:58 +0300

    python arrays

--- python_arrays.py
+++ python_arrays.py
@@ -1,1 +1,11 @@
 #python arrays
+#Arrays are used to store multiple values in one single variable:
+#     {'source1': 'data1', 'source2': 'data2'}
+#     {'source3': 'data3', 'source4': 'data4'}
+#     {'source5': 'data5', 'source6': 'data6'}
+# ]"""
+# import numpy as np
+# raw_data = [np.array([1, 2, 3]), np.array([4, 5, 6]), np.array([7, 8, 9])]
+# processed_data = [data * 2 for data in raw_data]  # Example transformation
+# print(processed_data)  # Output: [array([2, 4, 6]), array([ 8, 10, 12]), array([14, 16, 18])]
+# Example of using numpy arrays for data processing
