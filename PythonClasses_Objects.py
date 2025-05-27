ffd8a03f00b628e5b75b32e9e8e3b2943642cf3b
JuniorCarti <ridgejunior204@gmail.com>
2025-05-27 14:35:15 +0300

    Python classes and objects

--- PythonClasses_Objects.py
+++ PythonClasses_Objects.py
@@ -1,1 +1,6 @@
 #python  classes and objects
+#A Class is like an object constructor, or a "blueprint" for creating objects.
+#To create a class, use the keyword class:
+#Create a class named MyClass, with a property named x:
+class myClass:
+    x = 5
