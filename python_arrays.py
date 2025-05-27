c9bb0c10f7c14cee47a878279c7986597440d58b
JuniorCarti <ridgejunior204@gmail.com>
2025-05-27 13:59:02 +0300

    python arrays

--- python_arrays.py
+++ python_arrays.py
@@ -9,3 +9,16 @@
 # processed_data = [data * 2 for data in raw_data]  # Example transformation
 # print(processed_data)  # Output: [array([2, 4, 6]), array([ 8, 10, 12]), array([14, 16, 18])]
 # Example of using numpy arrays for data processing
+
+#What is an Array?
+# An array is a data structure that can hold multiple values of the same type.
+# In Python, the most common way to create arrays is by using lists or the numpy library.
+#An array is a special variable, which can hold more than one value at a time.
+#If you have a list of items (a list of car names, for example), 
+# storing the cars in single variables could look like this:
+cars = ['Toyota', 'Honda', 'Ford', 'Chevrolet']
+# However, this is not efficient for larger datasets.
+# Instead, you can use an array to store multiple values in a single variable.
+# Example of using a list to store car information
+
+cars = [   {'make': 'Toyota', 'model': 'Camry', 'year': 2020},]
