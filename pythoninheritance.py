93e65d04602ef1716a4bba4681612de14b096caf
JuniorCarti <ridgejunior204@gmail.com>
2025-05-28 15:43:38 +0300

    Python inheritance

--- pythoninheritance.py
+++ pythoninheritance.py
@@ -57,3 +57,14 @@ class Student(Person):
     self.graduationyear = year
 
 x = Student("Mike", "Olsen", 2019)
+
+#Add Methods
+#Add a method called welcome to the Student class:
+class Student(Person):
+  def __init__(self, fname, lname, year):
+    super().__init__(fname, lname)
+    self.graduationyear = year
+
+  def welcome(self):
+    print("Welcome", self.firstname, self.lastname, "to the class of", self.graduationyear)
+    
