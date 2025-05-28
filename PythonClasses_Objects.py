628b8435e39675765ef677f5686b58c825cc2724
JuniorCarti <ridgejunior204@gmail.com>
2025-05-28 14:05:20 +0300

    Bank Account System

--- PythonClasses_Objects.py
+++ PythonClasses_Objects.py
@@ -172,3 +172,8 @@ class Laptops:
 
 #Examples of real scenarios
 #1. Bank Account System
+
+class BankAccount:
+    def __init__(self, account_holder, balance=0):
+        self.account_holder = account_holder
+        self.balance = balance
