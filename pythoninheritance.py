27b3dcdae0e556accaef92eb298e9ac228d0acc9
JuniorCarti <ridgejunior204@gmail.com>
2025-05-28 15:02:20 +0300

    Python inheritance

--- pythoninheritance.py
+++ pythoninheritance.py
@@ -12,3 +12,6 @@ class Person:
     self.lastname = lname
   def printname(self):
     print(self.firstname, self.lastname) 
+#Use the Person class to create an object, and then execute the printname method:
+x = Person("Ridge", "Junior")
+x.printname()
