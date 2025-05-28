e3b96f776c434b21ad74fc39697aef9dd1b6133a
JuniorCarti <ridgejunior204@gmail.com>
2025-05-28 14:12:52 +0300

    Bank account system

--- PythonClasses_Objects.py
+++ PythonClasses_Objects.py
@@ -188,4 +188,8 @@ class BankAccount:
             self.balance -= amount
         print(f"Withdrew ${amount}. Remaining balance: ${self.balance}")
 
+    def __str__(self):
+        return f"Account Holder: {self.account_holder} \nBalance: ${self.balance}"
+    
+
 
