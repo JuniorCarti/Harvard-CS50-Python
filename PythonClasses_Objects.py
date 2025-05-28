c4180b24b65de1520afacf48d1872a59b5e85f46
JuniorCarti <ridgejunior204@gmail.com>
2025-05-28 13:47:50 +0300

    Python Classes And Objects

--- PythonClasses_Objects.py
+++ PythonClasses_Objects.py
@@ -127,3 +127,9 @@ Cr1 = Courses("Diploma in Electrical Engineering", 56000, 3, "Eldoret Polytechni
 Cr1.myfunc()  
 
 #The self Parameter
+#The self parameter is a reference to the current instance of the class, 
+#and is used to access variables that belong to the class.
+#It does not have to be named self, you can call it whatever you like, 
+# but it has to be the first parameter of any function in the class:
+
+
