d49d36677c762637456127b69496f94fb55850ae
JuniorCarti <ridgejunior204@gmail.com>
2025-06-03 16:10:19 +0300

    python_scope

--- python_scope.py
+++ python_scope.py
@@ -1,1 +1,15 @@
 #python scope
+#a variable is a name that refers to a value
+#a variable is only available from inside the region it is created
+# and the variables are available from inside any function that is in the same region
+# Example of variable scope in Python
+def outer_function():
+    x = "local"
+    
+    def inner_function():
+        nonlocal x  # Access the variable from the outer function
+        x = "nonlocal"
+        print("Inner function:", x)  # Output: Inner function: nonlocal
+    
+    inner_function()
+    print("Outer function:", x)  # Output: Outer function: nonlocal
