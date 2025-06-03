51d99166c9e616ee5f6acdda91b6f36bb6f51d78
JuniorCarti <ridgejunior204@gmail.com>
2025-06-03 16:38:34 +0300

    python_scope

--- python_scope.py
+++ python_scope.py
@@ -93,3 +93,12 @@ def myfunc():
   print(x)
 
 myfunc()
+#Function Inside Function
+#As explained in the example above, the variable x is not available outside the function, 
+#but it is available for any function inside the function:
+def myfunc():
+    x = 300
+    def myinnerfunc():
+        print(x)
+    myinnerfunc()
+myfunc()
