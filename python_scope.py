051c1c6cc31208745eacc324047d6bc86abb45d9
JuniorCarti <ridgejunior204@gmail.com>
2025-06-03 16:37:13 +0300

    python_scope

--- python_scope.py
+++ python_scope.py
@@ -87,4 +87,9 @@ def outer_nonlocal():
 #A variable created inside a function belongs to the local scope of that function, 
 # and can only be used inside that function.
 #Example
-#A variable created inside a function is available inside that function:
+#A variable created inside a function is available inside that function:\
+def myfunc():
+  x = 300
+  print(x)
+
+myfunc()
