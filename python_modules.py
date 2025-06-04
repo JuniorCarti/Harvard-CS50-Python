bb3507420d6683cc5ed5e9350958a3893242f406
JuniorCarti <ridgejunior204@gmail.com>
2025-06-04 22:11:43 +0300

    Python modules

--- python_modules.py
+++ python_modules.py
@@ -1,2 +1,4 @@
 #python modules
 # A module is a file containing Python code. It can define functions, classes, and variables.
+# Modules allow you to logically organize your Python code and reuse it across different programs.
+# To use a module, you can import it using the import statement.
