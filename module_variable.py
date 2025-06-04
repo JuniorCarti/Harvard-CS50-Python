e4bc83af453656fa39798d5864b295f615a74bca
JuniorCarti <ridgejunior204@gmail.com>
2025-06-04 22:27:05 +0300

    variables in module

--- module_variable.py
+++ module_variable.py
@@ -1,2 +1,7 @@
 #module variable.py
 # A variable created in a module is available in that module and can be accessed by importing the module.
+person1 = {
+  "name": "John",
+  "age": 36,
+  "country": "Norway"
+}
