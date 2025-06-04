c95bb12cf9724fe9b464df0846d1a7069393320a
JuniorCarti <ridgejunior204@gmail.com>
2025-06-04 22:23:56 +0300

    variables in module

--- mymodule.py
+++ mymodule.py
@@ -19,3 +19,12 @@ mymodule.greeting("Charlie")
 # Import the module named mymodule and call the greeting function
 import mymodule 
 mymodule.greeting("David")
+
+
+#Variables in Module
+# A variable created in a module is available in that module and can be accessed by importing the module.
+# Example of a variable in a module
+module_variable = "This is a module variable"
+def get_module_variable():
+    return module_variable
+# Example of using the module variable
