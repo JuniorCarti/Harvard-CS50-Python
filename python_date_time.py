0e8efb333783709b494bb36152a696ea84771c46
JuniorCarti <ridgejunior204@gmail.com>
2025-06-05 22:31:18 +0300

    Python date time

--- python_date_time.py
+++ python_date_time.py
@@ -4,3 +4,9 @@ import datetime
 # Example of using the datetime module to get the current date and time
 def get_current_datetime():
     return datetime.datetime.now()
+# Example of using the get_current_datetime function
+def main_datetime():
+    current_datetime = get_current_datetime()
+    print(f"Current Date and Time: {current_datetime}")
+if __name__ == "__main__":
+    main_datetime()
