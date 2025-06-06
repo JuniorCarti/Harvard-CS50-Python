b580db7b0cd1c8264ea7767056e35a307ea146fb
JuniorCarti <ridgejunior204@gmail.com>
2025-06-06 21:52:28 +0300

    python math

---
+++ python_math.py
@@ -0,0 +1,6 @@
+#python math
+# Python Math
+import math
+# Example of using the math module to calculate the square root of a number
+def calculate_square_root(number):
+    return math.sqrt(number)
