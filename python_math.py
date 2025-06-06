bf6069c8a794db445cb0f690e4da3ff8df96dcb9
JuniorCarti <ridgejunior204@gmail.com>
2025-06-06 21:53:20 +0300

    python math

--- python_math.py
+++ python_math.py
@@ -11,3 +11,7 @@ def main_square_root():
     print(f"Square Root of {number}: {square_root}")    
 if __name__ == "__main__":
     main_square_root()
+# Example of using the math module to calculate the factorial of a number
+def calculate_factorial(number):
+    return math.factorial(number)
+# Example of using the calculate_factorial function
