4492526bb3ecde59a682a78682703239aa43acbe
JuniorCarti <ridgejunior204@gmail.com>
2025-06-06 21:52:50 +0300

    python math

--- python_math.py
+++ python_math.py
@@ -4,3 +4,8 @@ import math
 # Example of using the math module to calculate the square root of a number
 def calculate_square_root(number):
     return math.sqrt(number)
+# Example of using the calculate_square_root function
+def main_square_root():
+    number = 16
+    square_root = calculate_square_root(number)
+    print(f"Square Root of {number}: {square_root}")    
