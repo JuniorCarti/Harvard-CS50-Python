ff4dacf5a1fe9dd98d9f700203eb98068b708b11
JuniorCarti <ridgejunior204@gmail.com>
2025-06-09 22:59:55 +0300

    python json

--- python_json.py
+++ python_json.py
@@ -2,3 +2,11 @@
 # https://docs.python.org/3/library/json.html
 import json
 # json.dumps() converts a Python object into a JSON string
+def json_dumps_example():
+    data = {
+        "name": "John",
+        "age": 30,
+        "city": "New York"
+    }
+    json_string = json.dumps(data)
+    print("JSON String:", json_string)
