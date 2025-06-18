fdf96fe2c436e2964d7ef95c834c8fb89b6e6b39
JuniorCarti <ridgejunior204@gmail.com>
2025-06-18 23:06:49 +0300

    python string formatting

--- python_string_formatting.py
+++ python_string_formatting.py
@@ -1,1 +1,9 @@
 # 1. f-strings (Formatted String Literals) – Best & Fastest
+name = "Ridge"
+age = 25
+print(f"My name is {name} and I am {age} years old.")
+
+
+#str.format() Method
+template = "Hello {}, your score is {}"
+print(template.format("Ridge", 87))
