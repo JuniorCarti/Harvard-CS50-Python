8ec54a6add2b2016a888e472ccef457e9b9abc5a
JuniorCarti <ridgejunior204@gmail.com>
2025-06-18 22:41:43 +0300

    python try except

--- Python Try Except.py
+++ Python Try Except.py
@@ -21,3 +21,10 @@ except:
 
   #Else
   #You can use the else keyword to define a block of code to be executed if no errors were raised:
+
+try:
+  print("Hello")
+except:
+  print("Something went wrong")
+else:
+  print("Nothing went wrong")
