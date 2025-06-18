4b063855084f0b397a6b8816bb63e67400eac8f0
JuniorCarti <ridgejunior204@gmail.com>
2025-06-18 22:45:51 +0300

    python try except

--- Python Try Except.py
+++ Python Try Except.py
@@ -57,3 +57,10 @@ x = -1
 
 if x < 0:
   raise Exception("Sorry, no numbers below zero")
+
+#The raise keyword is used to raise an exception.
+#You can define what kind of error to raise, and the text to print to the user.
+x = "hello"
+
+if not type(x) is int:
+  raise TypeError("Only integers are allowed")
