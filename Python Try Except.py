89383448944717faae30f62d68568b4f3ef12c1c
JuniorCarti <ridgejunior204@gmail.com>
2025-06-18 22:44:51 +0300

    python try except

--- Python Try Except.py
+++ Python Try Except.py
@@ -49,3 +49,11 @@ try:
     f.close()
 except:
   print("Something went wrong when opening the file")
+
+#Raise an exception
+#As a Python developer you can choose to throw an exception if a condition occurs.
+#To throw (or raise) an exception, use the raise keyword.
+x = -1
+
+if x < 0:
+  raise Exception("Sorry, no numbers below zero")
