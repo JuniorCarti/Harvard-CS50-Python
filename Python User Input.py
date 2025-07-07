acb41555db9beaba28c7116fa889d511961a704e
JuniorCarti <ridgejunior204@gmail.com>
2025-07-07 19:56:37 +0300

    Python User Input

--- Python User Input.py
+++ Python User Input.py
@@ -2,3 +2,7 @@
 #Python allows for user input.
 #That means we are able to ask the user for input.
 #The following example asks for your name, and when you enter a name, it gets printed on the screen:
+#user input
+
+name = input("What's your name? ")
+print("Hello, " + name + "!")
