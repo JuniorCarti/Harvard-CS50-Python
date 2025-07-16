63b85a636ee857441eddeae8d29311d687f1f2fa
JuniorCarti <ridgejunior204@gmail.com>
2025-07-16 15:04:28 +0300

    Python inheritance

--- pythoninheritance.py
+++ pythoninheritance.py
@@ -68,3 +68,18 @@ class Student(Person):
   def welcome(self):
     print("Welcome", self.firstname, self.lastname, "to the class of", self.graduationyear)
     
+class BankAccount:
+    def __init__(self, account_number, balance):
+        self.account_number = account_number
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+        print(f"Deposited {amount}. New balance is {self.balance}.")
+
+    def withdraw(self, amount):
+        if amount > self.balance:
+            print("Insufficient funds.")
+        else:
+            self.balance -= amount
+            print(f"Withdrew {amount}. New balance is {self.balance}.")
